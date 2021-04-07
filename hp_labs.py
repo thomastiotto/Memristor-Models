7f42df279efcfcb59f557324d5b9da5a3c54271b
Thomas <thomas.tiotto@outlook.com>
2021-04-07 11:17:50 +0200

    - reformatted memristor model into a class - made voltage and window into functions - tweaked memristor parameters to reproduce Yakopcic's thesis results - progressbar on iterative simulations - removed odeint solver

--- hp_labs.py
+++ hp_labs.py
@@ -4,6 +4,7 @@ import matlab.engine
 from block_timer.timer import Timer
 import scipy.signal
 from scipy.integrate import odeint, solve_ivp
+from progressbar import progressbar
 
 ##### MATLAB EXAMPLE
 eng = matlab.engine.start_matlab()
@@ -32,29 +33,57 @@ class input_voltage():
         return v
 
 
+def no_window( **kwargs ):
+    return 1
+
+
+def joglekar( **kwargs ):
+    x = kwargs[ "x" ]
+    p = kwargs[ "p" ] if "p" in kwargs else 10
+    
+    return 1 - np.power( np.multiply( 2, x ) - 1, 2 * p )
+
+
+def biolek( **kwargs ):
+    x = kwargs[ "x" ]
+    i = kwargs[ "i" ]
+    p = kwargs[ "p" ] if "p" in kwargs else 10
+    
+    return 1 - np.power( x - np.heaviside( -i, 1 ), 2 * p )
+
+
 class hp_labs():
-    def __init__( self, input ):
+    def __init__( self, input, window_function, **kwargs ):
         self.V = input.V
+        self.F = window_function
+        
+        self.D = kwargs[ "D" ] if "D" in kwargs else 27e-9
+        self.R_ON = kwargs[ "R_ON" ] if "R_ON" in kwargs else 1e2
+        self.R_OFF = kwargs[ "R_OFF" ] if "R_OFF" in kwargs else 16e3
+        self.R_INIT = kwargs[ "R_INIT" ] if "R_INIT" in kwargs else 11e3
+        self.m_D = kwargs[ "m_D" ] if "m_D" in kwargs else 1e-14
     
     def I( self, t, x ):
         return self.V( t ) / (np.multiply( R_ON, x ) + np.multiply( R_OFF, (np.subtract( 1, x )) ))
     
     def mu_D( self, t, x ):
-        return ((m_D * R_ON) / np.power( D, 2 )) * self.I( t, x ) * self.F( x )
-    
-    def F( self, x ):
-        return (1 - np.power( np.multiply( 2, x ) - 1, 2 * p ))
+        i = self.I( t, x )
+        
+        return ((m_D * R_ON) / np.power( D, 2 )) * i * self.F( x=x, i=i )
 
 
 t_min = 0
 t_max = 8
-N = 1000
+N = 10000
 dt = (t_max - t_min) / N
 time = np.arange( t_min, t_max + dt, dt )
-input_shape = "sine"
-# input_shape = "triangle"
+# input_shape = "sine"
+input_shape = "triangle"
+# window_function = no_window
+# window_function = joglekar
+window_function = biolek
 
-D = 10e-9
+D = 27e-9
 R_ON = 1e2
 R_OFF = 16e3
 R_INIT = 11e3
@@ -65,24 +94,37 @@ x0 = (R_OFF - R_INIT) / (R_OFF - R_ON)
 x_euler = [ x0 ]
 x_rk4 = [ x0 ]
 current = [ 0.0 ]
+solutions = [ ]
 
-memristor = hp_labs( input_voltage( input_shape, t_max ) )
+memristor = hp_labs( input_voltage( input_shape, t_max ), window_function )
 dxdt = memristor.mu_D
 V = memristor.V
 I = memristor.I
+F = memristor.F
 
 # Solve ODE iteratively using Euler's method
 with Timer( title="Euler" ):
-    for t in time[ :-1 ]:
-        current.append( I( t, x_euler[ -1 ] ) )
-        x_euler.append( x_euler[ -1 ] + dxdt( t, x_euler[ -1 ] ) * dt )
+    print( "Running Euler" )
+    
+    for t in progressbar( time[ :-1 ] ):
+        # print(
+        #         "Euler", "t", '{:.6e}'.format( t ),
+        #         "V", '{:.6e}'.format( V( t ) ),
+        #         "I", '{:.6e}'.format( I( t, x_euler[ -1 ] ) ),
+        #         "F", '{:.6e}'.format( F( x=x_euler[ -1 ], i=I( t, x_euler[ -1 ] ) ) ),
+        #         "x", '{:.6e}'.format( x_euler[ -1 ] ),
+        #         "dx", '{:.6e}'.format( dxdt( t, x_euler[ -1 ] ) )
+        #         )
         
-        # print( "Euler", "t", '{:.6e}'.format( t ), "I", '{:.6e}'.format( I( t, x_euler[ -1 ] ) ), "dx",
-        #        '{:.6e}'.format( dxdt( t, x_euler[ -1 ] ) ) )
+        current.append( I( t, x_euler[ -1 ] ) )
+        
+        x_euler.append( x_euler[ -1 ] + dxdt( t, x_euler[ -1 ] ) * dt )
+    solutions.append( (x_euler, time, "Euler") )
 
 # Solve ODE iteratively using Runge-Kutta's method
-with Timer( title="Runge-Kutta" ):
-    for t in time[ :-1 ]:
+with Timer( title="Runge-Kutta RK4" ):
+    print( "Running Runge-Kutta RK4" )
+    for t in progressbar( time[ :-1 ], redirect_stdout=True ):
         current.append( I( t, x_rk4[ -1 ] ) )
         
         k1 = dxdt( t, x_rk4[ -1 ] )
@@ -90,26 +132,20 @@ with Timer( title="Runge-Kutta" ):
         k3 = dxdt( t + dt / 2, x_rk4[ -1 ] + dt * k2 / 2 )
         k4 = dxdt( t + dt, x_rk4[ -1 ] + dt * k3 )
         x_rk4.append( x_rk4[ -1 ] + dt * (k1 + 2 * k2 + 2 * k3 + k4) / 6 )
-        
-        print( "RK4", "t", '{:.6e}'.format( t ), "I", '{:.6e}'.format( I( t, x_rk4[ -1 ] ) ), "dx",
-               '{:.6e}'.format( dxdt( t, x_rk4[ -1 ] ) ) )
+    solutions.append( (x_rk4, time, "Runge-Kutta") )
 
-# Solve ODE
-with Timer( title="odeint" ):
-    x_odeint = odeint( dxdt, x0, time )
+# Solve ODE with solver
 with Timer( title="solve_ivp" ):
+    print( "Running solve_ivp" )
     x_solve_ivp = solve_ivp( dxdt, (t_min, t_max), [ x0 ], method="LSODA", t_eval=time )
+    solutions.append( (x_solve_ivp.y[ 0, : ], x_solve_ivp.t, "solve_ivp") )
 
-for x, t, title in zip(
-        [ x_euler, x_rk4, x_odeint[ :, 0 ], x_solve_ivp.y[ 0, : ] ],
-        [ time, time, time, x_solve_ivp.t ],
-        [ "Euler", "RK4", "odeint", "solve_ivp" ]
-        ):
+for x, t, title in solutions:
     fig, axes = plt.subplots( 1, 2 )
     ax11 = axes[ 0 ]
     ax11.plot( t, I( t, x ) * 1e6, color="b" )
     ax11.set_ylabel( r'Current ($\mu A$)', color='b' )
-    ax11.set_ylim( [ -150, 150 ] )
+    # ax11.set_ylim( [ -150, 150 ] )
     ax11.tick_params( 'y', colors='b' )
     ax12 = ax11.twinx()
     ax12.plot( t, V( t ), color="r" )
