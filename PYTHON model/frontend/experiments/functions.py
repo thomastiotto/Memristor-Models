3419de96409f53c5eb03be7901c3201024e12250
dmkhitaryan <davtot111@icloud.com>
2022-08-10 13:39:05 +0200

    Removing frequency dependency, fixing the discrepancy between the time and voltage arrays.

--- PYTHON model/frontend/experiments/functions.py
+++ PYTHON model/frontend/experiments/functions.py
@@ -22,24 +22,25 @@ def solver2(f, time, dt, iv, v, args=[]):
 # Produces the voltage pulses based on the given inputs.
 def interactive_iv(iptVs, dt):
     t = 0
-    frequency = int(1 / dt)
+    print("dt: ", dt)
     for iptV in iptVs.values():
         for j in range(0, int(iptV['n_cycles'])):
             if j == 0 and iptVs["1"] == iptV:
-                t, v_total = generate_wave(iptV, frequency, t)
+                t, v_total = generate_wave(iptV, dt, t)
             else:
-                t, v_total = generate_wave(iptV, frequency, t, v_total)
+                t, v_total = generate_wave(iptV, dt, t, v_total)
 
-    time = np.arange(0, t+dt, dt)
+    time = np.linspace(0, t+dt, len(v_total))
     return time, v_total
 
 
-def generate_wave(iv, frequency, t, base=None):
+def generate_wave(iv, dt, t, base=None):
     base = np.array([0]) if base is None else base
     t += (iv["t_rise"] + iv["t_fall"] + iv["t_on"] + iv["t_off"])
-    v1 = np.linspace(iv["V_off"], iv["V_on"], int(iv["t_rise"] * frequency))
-    v2 = iv["V_on"] * np.ones(int(iv["t_on"] * frequency))
-    v3 = np.linspace(iv["V_on"], iv["V_off"], int(iv["t_fall"] * frequency))
-    v4 = np.array([]) if iv["t_off"] == 0 else iv["V_off"] * np.ones(int(iv["t_off"] * frequency))
+    v1 = np.linspace(iv["V_off"], iv["V_on"], round(iv["t_rise"] * 1/dt))
+    v2 = iv["V_on"] * np.ones(round(iv["t_on"] * 1/dt))
+    v3 = np.linspace(iv["V_on"], iv["V_off"], round(iv["t_fall"] * 1/dt))
+    v4 = np.array([]) if iv["t_off"] == 0 else iv["V_off"] * np.ones(round(iv["t_off"] * 1/dt))
     vtotal = np.concatenate((base, v1, v2, v3, v4))
     return t, vtotal
+
