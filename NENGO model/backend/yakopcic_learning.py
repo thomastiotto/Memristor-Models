478884c6060ffe6cd616389f2a3a2a81601fbe21
Thomas Tiotto <thomas.tiotto@outlook.com>
2022-10-07 18:46:17 +0200

    - better initialisation of x0

--- NENGO model/backend/yakopcic_learning.py
+++ NENGO model/backend/yakopcic_learning.py
@@ -311,9 +311,9 @@ class SimmPES(Operator):
                 self.x = np.select([self.x < 0, self.x > 1], [0, 1], default=self.x)
 
                 # Calculate the current and the resistance for the devices
-                i = current(np.zeros(V.shape,dtype=float)*-1, self.x, self.gmax_p, self.bmax_p,
+                i = current(np.zeros(V.shape, dtype=float) * -1, self.x, self.gmax_p, self.bmax_p,
                             self.gmax_n, self.bmax_n, self.gmin_p, self.bmin_p, self.gmin_n, self.bmin_n)
-                r = np.divide(np.zeros(V.shape,dtype=float)*-1, i, out=np.zeros(V.shape, dtype=float), where=i != 0)
+                r = np.divide(np.zeros(V.shape, dtype=float) * -1, i, out=np.zeros(V.shape, dtype=float), where=i != 0)
                 # Clip the value of resistances beyond the [r_min, r_max] range
                 r = np.select([r < self.r_min, r > self.r_max], [r_min, r_max], default=r)
 
@@ -385,7 +385,7 @@ def build_mpes(model, mpes, rule):
     gmax_p = np.random.normal(0.0004338454236, 0.00006433347881, (encoders.shape[0], acts.shape[0]))
     gmin_n = np.random.normal(1.6806214980624974e-07, 0.000001269628401, (encoders.shape[0], acts.shape[0]))
     gmin_p = np.random.normal(0.03135053798, 0.01128684089, (encoders.shape[0], acts.shape[0]))
-    x0 = np.random.normal(0.5, 0.5 * 0.1, (encoders.shape[0], acts.shape[0]))
+    x0 = get_truncated_normal(0.6251069761800688, 0.6251069761800688 * 0.15, 0, 1, encoders.shape[0], acts.shape[0])
     xn = np.random.normal(0.1433673316, 0.007340350194, (encoders.shape[0], acts.shape[0]))
     xp = np.random.normal(0.11, 0, (encoders.shape[0], acts.shape[0]))
 
