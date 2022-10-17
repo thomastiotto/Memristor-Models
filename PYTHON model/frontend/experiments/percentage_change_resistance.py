e15a164fa8acef279c1461d24f82354433111a10
Thomas Tiotto <thomas.tiotto@outlook.com>
2022-10-17 18:05:10 +0200

    - added percentage_change_resistance.py to calculate the strength of individual pulses

---
+++ PYTHON model/frontend/experiments/percentage_change_resistance.py
@@ -0,0 +1,65 @@
+import scipy.stats as stats
+import random
+
+import json
+from functions import *
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def one_step_yakopcic(voltage, x, readV, **params):
+    x = x + dxdt(voltage, x, model['Ap'], model['An'], model['Vp'], model['Vn'], model['xp'],
+                 model['xn'], model['alphap'], model['alphan'], 1) * model['dt']
+    if x < 0:
+        x = 0
+    if x > 1:
+        x = 1
+
+    i = current(readV, x,
+                params['gmax_p'], params['bmax_p'], params['gmax_n'], params['bmax_n'],
+                params['gmin_p'], params['bmin_p'], params['gmin_n'], params['bmin_n'])
+    r = readV / i
+
+    return x, r
+
+
+model = json.load(open('../../../fitted/fitting_pulses/regress_negative_xp_alphap-adjusted_ap_an'))
+iterations = 10
+
+x0 = 0.6251069761800688
+setV = 3.86621037038006
+# setV = 0
+resetV = -8.135891404816215
+resetV = -0.2
+# resetV = 0
+readV = -1
+
+# random.seed(8)
+
+
+x_p = x_n = x0
+R_p = []
+R_n = []
+X_p = [x_p]
+X_n = [x_n]
+
+for j in tqdm(range(iterations)):
+    x_p, r_p = one_step_yakopcic(setV, x_p, readV, **model)
+    R_p.append(r_p)
+print('\nStart value:', R_p[0], 'End value:', R_p[-1])
+set_efficacy = np.mean(np.diff(R_p) / np.abs(R_p[:-1]) * 100)
+print('Average resistance change with SET pulses:', set_efficacy, '%')
+
+for j in tqdm(range(iterations)):
+    x_n, r_n = one_step_yakopcic(resetV, x_n, readV, **model)
+    R_n.append(r_n)
+print('\nStart value:', R_n[0], 'End value:', R_n[-1])
+reset_efficacy = np.mean(np.diff(R_n) / np.abs(R_n[:-1]) * 100)
+print('Average resistance change with RESET pulses:', reset_efficacy, '%')
+print('RESET pulses are more effective than SET pulses by', ((set_efficacy - reset_efficacy) / set_efficacy) * 100, '%')
+
+fig, ax = plt.subplots()
+ax.plot(R_p)
+ax.twinx().plot(R_n)
+fig.show()
--- PYTHON model/frontend/experiments/two_memristor_exp.py
+++ PYTHON model/frontend/experiments/two_memristor_exp.py
@@ -104,11 +104,13 @@ def one_step_yakopcic(voltage, x, readV, **params):
 
 
 model = json.load(open('../../../fitted/fitting_pulses/regress_negative_xp_alphap-adjusted_ap_an'))
-iterations = 500
+iterations = 5000
 
 x0 = 0.6251069761800688
 setV = 3.86621037038006
-resetV = -8.135891404816215 / 10
+# setV = 0
+resetV = -8.135891404816215
+resetV = 0
 readV = -1
 
 # random.seed(8)
@@ -119,9 +121,9 @@ gaussian_plot = GaussianPlot()
 
 # (SET,RESET)/(neg. error,pos. error) pulse lengths obtained from simulating mPES.py
 for train_length in [
-    # (1, 1),
+    (1, 1),
     # (round(4.7370441230259654), round(4.691043103305757)),
-    (10.516604869146395, 10.514494153790514),
+    # (10.516604869146395, 10.514494153790514),
     # (100, 100)
 ]:
     print(train_length, ':')
@@ -130,26 +132,35 @@ for train_length in [
     x_n = get_truncated_normal(mean=x0, sd=x0 * 0.15, low=0, upp=1, out_size=1, in_size=1)[0, 0]
     R_p = []
     R_n = []
+    X_p = [x_p]
+    X_n = [x_n]
+
     for j in tqdm(range(int(iterations / (train_length[0] + train_length[1]) * 2))):
         pos_train_length = int(np.random.normal(train_length[0], train_length[0] * 0.15))
         neg_train_length = int(np.random.normal(train_length[1], train_length[1] * 0.15))
 
-        # Negative local error -> SET pulse excitatory, RESET pulse inhibitory -> Weight goes up
+        # Positive PES delta -> SET pulse excitatory, RESET pulse inhibitory -> Weight goes up
         if random.random() > 0.5:
             # pick random length
             for _ in range(pos_train_length):
                 x_p, r_p = one_step_yakopcic(setV, x_p, readV, **model)
                 x_n, r_n = one_step_yakopcic(resetV, x_n, readV, **model)
-                R_p.append(r_p)
-                R_n.append(r_n)
-        # Positive local error -> SET pulse inhibitory, RESET pulse excitatory -> Weight goes down
+        # Negative PES delta -> SET pulse inhibitory, RESET pulse excitatory -> Weight goes down
         else:
             for _ in range(neg_train_length):
                 x_p, r_p = one_step_yakopcic(resetV, x_p, readV, **model)
                 x_n, r_n = one_step_yakopcic(setV, x_n, readV, **model)
-                R_p.append(r_p)
-                R_n.append(r_n)
-    Wcombined = [x - y for x, y in zip([1 / x for x in R_p], [1 / x for x in R_n])]
+        try:
+            R_p.append(r_p)
+            R_n.append(r_n)
+            X_p.append(x_p)
+            X_n.append(x_n)
+        except:
+            pass
+    if len(R_p) == len(R_n):
+        Wcombined = [x - y for x, y in zip([1 / x for x in R_p], [1 / x for x in R_n])]
+    else:
+        Wcombined = [1 / x for x in R_p]
 
     mu = np.mean(Wcombined)
     sigma = np.std(Wcombined)
